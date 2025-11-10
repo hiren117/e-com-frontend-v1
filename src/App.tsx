2e63cf9ea6cb86998aabdbf4313fdeb99d22a2c3
Hiren Solanki <hirensolanki9697@gmail.com>
2025-11-10 16:41:27 +0530

    Initial Commit: setup vite + react + tailwind feat: added HomePage, react-alice-carousel, Navigation

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navigation from './customers/components/Navigation/Navigation'
+import HomePage from './customers/pages/HomePage/HomePage'
+
+function App() {
+
+  return (
+    <>
+    <Navigation />
+    <HomePage />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/customers/pages/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import HomeCarousel from "@/customers/components/HomeCarousel/MainCarousel"
+
+const HomePage = () => {
+  return (
+    <div>
+        <HomeCarousel />
+        <div>
+            other sections 
+            
+        </div>
+    </div>
+  )
+}
+export default HomePage;
